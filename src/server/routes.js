9dcb5664bc409d768a16e2dbe4ae1a6a10c76cef
Vianney <vianneyjchin@gmail.com>
2019-12-11 21:20:35 -0800

    post a shortened url

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,6 +1,33 @@
+const dns = require('dns')
+const checkIfShortIdExists = require('./util')
+const shortenUrl = require('./util')
+
 module.exports = (app, path) => {
   app.get('/', (_, res) => {
     const htmlPath = path.join(__dirname, '../client', 'index.html')
     res.sendFile(htmlPath)
+  })
+
+  app.post('/new', (req, res) => {
+    let originalUrl
+    try {
+      originalUrl = new URL(req.body.url)
+    } catch (error) {
+      res.status(404).send({ error: 'URL is invalid.' })
+    }
+    dns.lookup(originalUrl.hostname, err => {
+      if (err) {
+        return res.status(404).send({ error: 'URL is invalid.' })
+      }
+    })
+    shortenUrl(req.app.locals.db, originalUrl.href)
+      .then(result => {
+        const doc = result.value
+        res.json({
+          original_url: doc.original_url,
+          short_id: doc.short_id
+        })
+      })
+      .catch(console.error)
   })
 }
