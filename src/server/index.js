d68bc5b0c3158c6e76053a73e2ecf1b5296c94bd
Vianney <vianneyjchin@gmail.com>
2019-12-11 17:44:54 -0800

    Enable database

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const path = require('path')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+require('./db')(app)
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port: ${PORT}.`)
+})
